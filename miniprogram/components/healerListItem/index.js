28eb3626808ce9ba37acd10e4bb8958f3b98a4dd
VincentCorleone <707119577@qq.com>
2022-02-09 02:28:05 +0800

    manifest the home page of the booking system

---
+++ miniprogram/components/healerListItem/index.js
@@ -0,0 +1,38 @@
+// components/healerListItem/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    avatarUrl: {
+      type: String,
+      value: "/images/superconductor.jpeg"
+    },
+    healerName: {
+      type: String,
+      value: "Superconductor"
+    },
+    title: {
+      type: String,
+      value: "生物动力学头荐骨手触疗愈执行"
+    },
+    titleLevel: {
+      type: String,
+      value: "徒"
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
