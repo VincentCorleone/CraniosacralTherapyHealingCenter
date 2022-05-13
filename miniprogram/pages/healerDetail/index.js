4479d19ce6a71222f4c4b1ad974433b1fcc80a9e
VincentCorleone <707119577@qq.com>
2022-05-14 03:49:05 +0800

    2022年05月08日21:24:35~2022年05月14日03:48:59 添加无交互功能的地点控件pickerview

--- miniprogram/pages/healerDetail/index.js
+++ miniprogram/pages/healerDetail/index.js
@@ -5,16 +5,58 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    multiIndex: [0, 0],
+    multiArray: [[], []],
+    timeObject: [
+      { 
+        dayValue: "今天", 
+        timeSpans: 
+          ['9:00-9:50', 
+          '10:00-10:50', 
+          '14:00-14:50']
+      },
+      { 
+        dayValue: "明天", 
+        timeSpans: 
+          ['21:00-21:50', 
+          '10:00-10:50', 
+          '14:00-14:50']
+      },
+    ],
+    locationObject: [{}]
   },
 
+  bindMultiPickerColumnChange: function(e) {
+    console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
+    switch (e.detail.column) {
+      case 0:
+        this.data.multiArray[1] = this.data.timeObject[e.detail.value].timeSpans;
+        this.data.multiIndex[0] = e.detail.value;
+        this.data.multiIndex[1] = 0;
+        break;
+      case 1:
+        this.data.multiIndex[1] = e.detail.value;
+        break;
+    }
+
+
+    
+    this.setData(this.data);
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    Object.keys(this.data.timeObject).forEach((value, index) => {
+      this.data.multiArray[0].push(this.data.timeObject[index].dayValue);
+      console.log(this.data.timeObject[index].dayValue);
+    });
+    console.log(this.data.multiArray[0]);
+    // this.data.multiArray[0] = collection;
+    this.data.multiArray[1] = this.data.timeObject[0].timeSpans;
 
+    this.setData(this.data);
   },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
