5a27a2f16d4be8c51870503fdddc7529c544c7eb
VincentCorleone <707119577@qq.com>
2022-06-30 22:00:38 +0800

    增加“正在开发”展示页

---
+++ miniprogram/components/toDevelop/toDevelop.js
@@ -0,0 +1,23 @@
+// components/toDevelop/toDevelop.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
