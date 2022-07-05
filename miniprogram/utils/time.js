60d01c2bd92ba62b7687eaae14d8cf694bb76832
VincentCorleone <707119577@qq.com>
2022-07-05 16:25:38 +0800

    add function toString() to TimeInADay

--- miniprogram/utils/time.js
+++ miniprogram/utils/time.js
@@ -7,6 +7,11 @@ class TimeInADay {
   valueOf () {
     return this.hour * 60 + this.minute
   }
+
+  toString() {
+    const minuteT = this.minute<10? '0' + this.minute.toString() : this.minute.toString()
+    return `${this.hour}:${minuteT}`
+  }
 }
 
 module.exports = TimeInADay;
