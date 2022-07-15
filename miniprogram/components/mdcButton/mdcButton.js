4faeb4e186dd79ec821c0b71f230a80432779ea7
VincentCorleone <707119577@qq.com>
2022-07-15 18:31:56 +0800

    add mdcButton component

---
+++ miniprogram/components/mdcButton/mdcButton.js
@@ -0,0 +1,27 @@
+// components/mdcButton/mdcButton.ts
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  lifetimes: {
+    attached: function name(params) {
+
+    }
+  },
+  methods: {
+  }
+})
