4ea57d75daa00101d817fa716d54afe9503cde4b
VincentCorleone <707119577@qq.com>
2022-07-16 13:05:39 +0800

    留出statebar的位置

---
+++ miniprogram/components/osStateBarHolder/osStateBarHolder.ts
@@ -0,0 +1,33 @@
+// components/osStateBarHolder/osStateBarHolder.ts
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+    h: 0,
+  },
+
+  lifetimes: {
+    attached: async function(){
+      const statusBarHeight = (await wx.getSystemInfo()).statusBarHeight;
+      this.setData({
+        h: statusBarHeight
+      });
+      console.log(wx.getMenuButtonBoundingClientRect());
+    }
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
